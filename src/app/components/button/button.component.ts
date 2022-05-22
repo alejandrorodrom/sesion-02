102e9356d58a133f5e61110d911ce6003f04b792
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-05-22 13:58:45 -0500

    add button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+
+  @Output() clicked = new EventEmitter<string>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  emitEvent(): void {
+    console.log('Se emitio evento');
+    this.clicked.emit(`Se hizo click - ${new Date()}`);
   }
 
 }
--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -5,11 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+
+  title = 'Coffee Galaxy';
 
   constructor() { }
 
-  ngOnInit(): void {
+  changeTitle(text: string): void {
+    this.title = text;
   }
 
 }
