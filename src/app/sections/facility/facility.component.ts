795586533283bb3510bf969e2ff53620f7d5357b
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-05-22 12:23:21 -0500

    modify facility

--- src/app/sections/facility/facility.component.ts
+++ src/app/sections/facility/facility.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../../components/card/card.interface';
 
 @Component({
   selector: 'app-facility',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facility.component.css']
 })
 export class FacilityComponent implements OnInit {
+
+  readonly ourFacilities: Card[] = [
+    {
+      src: 'assets/images/icon-1.png',
+      alt: 'coffee',
+      title: 'varieties of coffees',
+      text: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Saepe, Adipisci!'
+    },
+    {
+      src: 'assets/images/icon-2.png',
+      alt: 'coffee',
+      title: 'coffee beans',
+      text: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Saepe, Adipisci!'
+    },
+    {
+      src: 'assets/images/icon-3.png',
+      alt: 'coffee',
+      title: 'breakfast and sweets',
+      text: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Saepe, Adipisci!'
+    },
+    {
+      src: 'assets/images/icon-4.png',
+      alt: 'coffee',
+      title: 'read to go coffee',
+      text: 'Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Saepe, Adipisci!'
+    }
+  ];
 
   constructor() { }
 
