2c2d21f6e2019dd0022022aa7ac31e0068f692f0
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-05-22 11:09:51 -0500

    add title component and section home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './sections/header/header.component';
 import { LinkComponent } from './components/link/link.component';
+import { HomeComponent } from './sections/home/home.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LinkComponent
+    LinkComponent,
+    HomeComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
